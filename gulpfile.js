db6ed07aab74358a73901c5314df1c8b7d456765
martinsvb <arzu.suleymanov.1990@hotmail.com>
2019-02-11 19:45:56 +0100

    Build optimalization

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,17 @@ const minify = require('gulp-minify');
 
 gulp.task('clean', () => del(['dist']));
 
-gulp.task('default', gulp.series('clean', (done) => {
-    gulp.src(['src/validation.js', 'src/form.js', 'src/send.js'])
-        .pipe(concat('scripts.js'))
+gulp.task('css', (done) => {
+    gulp.src(['css/reset.css', 'css/main.css', 'css/contactForm.css', 'css/responsive.css'])
+        .pipe(concat('styles.css'))
         .pipe(minify())
         .pipe(gulp.dest('dist'));
     done();
-}, (done) => {
-    gulp.src(['css/reset.css', 'css/main.css', 'css/contactForm.css', 'css/responsive.css'])
-        .pipe(concat('styles.css'))
+});
+
+gulp.task('default', gulp.series('clean', 'css', (done) => {
+    gulp.src(['src/validation.js', 'src/form.js', 'src/send.js'])
+        .pipe(concat('scripts.js'))
         .pipe(minify())
         .pipe(gulp.dest('dist'));
     done();
