0910e07c7495b3ae4be1ab020e99aee856f468e1
martinsvb <arzu.suleymanov.1990@hotmail.com>
2020-04-18 23:33:43 +0200

    Contact form opening update

--- src/form.js
+++ src/form.js
@@ -23,7 +23,35 @@ function inputChanged(e) {
 }
 
 function scrollToContactForm() {
+    var formArrow = document.querySelector('.contactFormArrow');
+    if (formArrow.classList.contains('contactFormArrowUp')) {
+        formArrow.classList.replace('contactFormArrowUp', 'contactFormArrowDown');
+    }
+    var form = document.querySelector('.contactForm');
+    form.classList.remove('contactFormHide');
+    setTimeout(function(){
+        form.style.opacity = 1;
+    }, 0);
     var contentEl = document.getElementById('content');
     var htmlEl = document.getElementsByTagName('html')[0];
     htmlEl.scrollTop = contentEl.offsetTop - 10;
+}
+
+function contactFormClicked() {
+    var arrow = 'contactFormArrow';
+    var formArrow = document.querySelector('.' + arrow);
+    var isArrowUp = formArrow.classList.contains('contactFormArrowUp');
+    var oldClass = isArrowUp ? arrow + 'Up' : arrow + 'Down';
+    var newClass = isArrowUp ? arrow + 'Down' : arrow + 'Up';
+    formArrow.classList.replace(oldClass, newClass);
+    var form = document.querySelector('.contactForm');
+    if (form.classList.contains('contactFormHide')) {
+        scrollToContactForm();
+    }
+    else {
+        form.style.opacity = 0;
+        setTimeout(function(){
+            form.classList.add('contactFormHide');
+        }, 500);
+    }
 }
