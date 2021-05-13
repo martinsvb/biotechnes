2f132216403ae3b2f9751fd11dc8e6f91202c21b
martinsvb <svobodamartin@centrum.cz>
2021-05-13 11:15:24 +0200

    Footer info text added

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,20 @@ const concat = require('gulp-concat');
 const del = require('del');
 const gulp = require('gulp');
 const minify = require('gulp-minify');
+const inject = require('gulp-inject-string');
+
+const distStyles = `styles-${Date.now().toString()}.css`;
 
 gulp.task('clean', () => del(['dist']));
 
 gulp.task('css', (done) => {
     gulp.src(['css/reset.css', 'css/main.css', 'css/infoText.css', 'css/contactForm.css', 'css/footer.css', 'css/responsive.css'])
-        .pipe(concat('styles.css'))
+        .pipe(concat(distStyles))
         .pipe(minify())
         .pipe(gulp.dest('dist'));
+    gulp.src('index.html')
+        .pipe(inject.replace(/styles-\d{13}.css/, distStyles))
+        .pipe(gulp.dest('./'));
     done();
 });
 
